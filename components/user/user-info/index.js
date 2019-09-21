fb4ee138d99e90c67c7b7a587756c444358d4843
LX <1804307732@qq.com>
2019-09-21 22:46:15 +0800

    add feat user-info

---
+++ components/user/user-info/index.js
@@ -0,0 +1,24 @@
+// components/user/user-info/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    userName: '缪泽航',
+    userAvaterPath: '/image/user.png',
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
