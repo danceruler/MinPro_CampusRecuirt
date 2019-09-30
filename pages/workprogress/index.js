8befcf6ef108458a20e7d8ffeae569e03a00fc2d
LX <1804307732@qq.com>
2019-09-30 17:04:07 +0800

    fix test case

--- pages/workprogress/index.js
+++ pages/workprogress/index.js
@@ -24,60 +24,27 @@ Page({
       })
     }
   },
-  async testapi() {
-    var that = this
-    var data = await {
-      "key": that.data.condition.key,
-      "type": that.data.condition.type,
-      "cityId": that.data.condition.city,
-      "isExistsIntroCode": that.data.condition.isExistsIntroCode,
-      "page": that.data.page,
-      "count": that.data.count,
-      "maxTime": that.data.maxTime,
-      "uid": app.globalData.userInfo.uid,
-      "userId": app.globalData.userInfo.id,
-      "requestTime": util.formatTime(new Date()),
-      "secret": app.createSecret()
+  async api() {
+    let request = (time) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(time)
+        }, time)
+      })
     }
-    await wx.request({
-      url: Apis.Urls.GetRecruits,
-      data: data,
-      method: 'post',
-      dataType: 'application/json',
-      success: function (res) {
-        console.log(res)
-      },
-      fail: function (res) { },
-      complete: function (res) { },
-    })
-    await console.log(1)
-    await wx.request({
-      url: Apis.Urls.GetRecruits,
-      data: data,
-      method: 'post',
-      dataType: 'application/json',
-      success: function (res) {
-        console.log(res)
-      },
-      fail: function (res) { },
-      complete: function (res) { },
-    })
-    await console.log(2)
-    await wx.request({
-      url: Apis.Urls.GetRecruits,
-      data: data,
-      method: 'post',
-      dataType: 'application/json',
-      success: function (res) {
-        console.log(res)
-      },
-      fail: function (res) { },
-      complete: function (res) { },
-    })
-    await console.log(3)
+    let res = []
+    let a1 = await request(1000)
+    res.push(a1)
+    let a2 = await request(500)
+    res.push(a2)
+    let a3 = await request(2000)
+    res.push(a3)
+    return res
   },
-  onLoad: function () {
-
+  async testapi() {
+    this.api().then(res => {
+      console.log(res)
+    })
   },
 
 })
